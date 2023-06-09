43cae0256b27c91c972b48d143eb4dde4953c829
Paz Valenzuela <pazvalenzu@gmail.com>
2023-06-08 20:54:17 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Grid from "@mui/material/Grid";
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import { pink } from '@mui/material/colors';
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import { pink } from "@mui/material/colors";
 import { Box } from "@mui/material";
-
-
 
 function App() {
   const audioRef = useRef(null);
@@ -72,102 +70,100 @@ function App() {
   };
 
   return (
-   
     <Box
-    sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "30%",
-      margin: "0 auto",
-      padding: "50px",
-      
-    }}
-  >
-    <Grid
-      container
       sx={{
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        border: "2px solid white",
-        borderRadius: "15px",
-        
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        padding: "50px",
       }}
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
     >
-      <Grid item xs={12}>
-        <h1 align="center">PlayList</h1>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          <Grid item >
-          <Box
-      component="img"
-      src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif"
-      alt="imagenrandom"
-      sx={{ width: 150, height: 150 }}
-    />          </Grid>
+      <Grid
+        container
+        sx={{
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          border: "2px solid white",
+          borderRadius: "15px",
+          maxWidth: "100%",
+          margin: "0 auto",
+          padding: "50px",
+        }}
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid item xs={12}>
+          <h1 align="center">PlayList</h1>
         </Grid>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Grid container justifyContent="center" spacing={2}>
-          <Grid item>
-            <Button
-              onClick={reproducirCancionAnterior}
-              disabled={!botoneraHabilitada}
-            >
-              <SkipPreviousIcon style={{ fontSize: 40, color: pink[500] }}/>
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button
-              onClick={reproducirSiguienteCancion}
-              disabled={!botoneraHabilitada}
-            >
-              <SkipNextIcon style={{ fontSize: 40, color: pink[500] }}/>
-            </Button>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item>
+              <Box
+                component="img"
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif"
+                alt="imagenrandom"
+                sx={{ width: 150, height: 150 }}/>{" "}
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          <Grid item>
-            <audio ref={audioRef} controls />
+        <Grid item xs={12} md={6}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item>
+              <Button
+                onClick={reproducirCancionAnterior}
+                disabled={!botoneraHabilitada}
+              >
+                <SkipPreviousIcon style={{ fontSize: 40, color: pink[500] }} />
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                onClick={reproducirSiguienteCancion}
+                disabled={!botoneraHabilitada}
+              >
+                <SkipNextIcon style={{ fontSize: 40, color: pink[500] }} />
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={2}>
-          <Grid item style={{ color: pink[500] }}>
-            <Button 
-              aria-controls="dropdown-menu"
-              aria-haspopup="true"
-              onClick={handleClick}
-              variant="contained"
-            >
-              Seleccionar canción
-            </Button>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item>
+              <audio ref={audioRef} controls />
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item style={{ color: pink[500] }}>
+              <Button
+                aria-controls="dropdown-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                variant="contained"
+              >
+                Seleccionar canción
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Menu
+            id="dropdown-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {listaNombresCanciones.map((nombre, index) => (
+              <MenuItem key={index} onClick={() => reproducirCancion(index)}>
+                {nombre}
+              </MenuItem>
+            ))}
+          </Menu>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Menu 
-          id="dropdown-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {listaNombresCanciones.map((nombre, index) => (
-            <MenuItem key={index} onClick={() => reproducirCancion(index)}>
-              {nombre}
-            </MenuItem>
-          ))}
-        </Menu>
-      </Grid>
-    </Grid>
     </Box>
- 
   );
 }
 
